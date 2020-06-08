c2ea0dff2b3d385c7acf523cd69bc2bda84fb67f
jx4n16 <44874422+jx4n16@users.noreply.github.com>
2020-06-08 13:23:57 +0200

    schnuppi

---
+++ schnuppertag.js
@@ -0,0 +1,15 @@
+/** 
+ * TODO: Variablen deklarieren
+*/
+
+/**
+ * TODO: Funktionen definieren 
+*/
+
+/** 
+ * TODO: Game Methode if-else switch
+*/
+
+/** 
+ * TODO: Win, Draw, Loose Funktionen
+*/
